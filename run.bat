69d15e55f0e0ce014bbcfc941e61f9fa0ad53829
Nasir Mustapha <teboy.com@gmail.com>
2026-03-30 12:39:22 +0100

    Remove unused files and scripts, including requirements, batch files, and HTML templates, to streamline the project structure.

--- run.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-:: Start the app
-start /MIN "" flask run
-timeout /t 5 /nobreak
-
-:: Launch Edge in fullscreen kiosk mode
-:: start "" "msedge" --kiosk --edge-kiosk-type=fullscreen --no-first-run --disable-features=msEdgeEnterpriseModePolicies http://127.0.0.1:5000
-start "" "msedge" --app=http://127.0.0.1:5000
